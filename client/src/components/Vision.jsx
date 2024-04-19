3cbb5fc14802db95f630e16690e494535c155963
Nawaf <nawafallharthii@gmail.com>
2024-04-19 21:10:27 +0300

    Updates

--- client/src/components/Vision.jsx
+++ client/src/components/Vision.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css'; // Update the relative path to the location of App.css
 import ScrollReveal from './ScrollReveal'; // Ensure you import the ScrollReveal component
+import firstGif from '/Users/nawaf/Desktop/AB3AAD_Project/client/src/img/xxx.gif'; // Import the GIF
 
 function VisionPage() {
   const randomSentence = 'To be a hub for those who are interested in integrating 3D printing in their speciality.';
@@ -10,6 +11,7 @@ function VisionPage() {
       <div className="vision-container">
         <h1 className="vision-title">Our Vision</h1>
         <p className="random-sentence">{randomSentence}</p>
+        <img src={firstGif} alt="Description of GIF" width="750" height="600"/> {/* Display the GIF with specified dimensions */}
       </div>
     </ScrollReveal>
   );
